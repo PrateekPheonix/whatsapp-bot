13fe4a5724a67aa7a58e0436d12dcb5179e4fc1a
Prateek Niket <prateekniket25@gmail.com>
2022-03-28 22:52:32 +0530

    v

--- index.js
+++ index.js
@@ -159,9 +159,3 @@ app.listen(port, () => {
 })
 
 
-// wbm.start({ session: false, showBrowser: false }).then(async () => {
-//     const phones = ['918699900057', '918446000022'];
-//     const message = 'Test: automated message';
-//     await wbm.send(phones, message);
-//     await wbm.end();
-// }).catch(err => console.log(err));
